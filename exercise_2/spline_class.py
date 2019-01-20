f722f0fc1f66950b3eaefdbd119bb7ade9149288
Qin He(Lily) <qin.he@tut.fi>
2019-01-20 02:06:01 +0000

    Upload New File

---
+++ exercise_2/spline_class.py
@@ -0,0 +1,450 @@
+"""
+Cubic hermite splines in 1d, 2d, and 3d
+
+Intentionally unfinished :)
+
+Related to FYS-4096 Computational Physics
+exercise 2 assignments.
+
+By Ilkka Kylanpaa on January 2019
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import linalg as LA
+
+
+"""
+Add basis functions p1,p2,q1,q2 here
+"""
+
+
+def init_1d_spline(x,f,h):
+    # now using complete boundary conditions
+    # with forward/backward derivative
+    # - natural boundary conditions commented
+    a=zeros((len(x),))
+    b=zeros((len(x),))
+    c=zeros((len(x),))
+    d=zeros((len(x),))
+    fx=zeros((len(x),))
+
+    # a[0]=1.0 # not needed
+    b[0]=1.0
+
+    # natural boundary conditions 
+    #c[0]=0.5
+    #d[0]=1.5*(f[1]-f[0])/(x[1]-x[0])
+
+    # complete boundary conditions
+    c[0]=0.0
+    d[0]=(f[1]-f[0])/(x[1]-x[0])
+    
+    for i in range(1,len(x)-1):
+        d[i]=6.0*(h[i]/h[i-1]-h[i-1]/h[i])*f[i]-6.0*h[i]/h[i-1]*f[i-1]+6.0*h[i-1]/h[i]*f[i+1]
+        a[i]=2.0*h[i]
+        b[i]=4.0*(h[i]+h[i-1])
+        c[i]=2.0*h[i-1]        
+    #end for
+
+    
+    b[-1]=1.0
+    #c[-1]=1.0 # not needed
+
+    # natural boundary conditions
+    #a[-1]=0.5
+    #d[-1]=1.5*(f[-1]-f[-2])/(x[-1]-x[-2])
+
+    # complete boundary conditions
+    a[-1]=0.0
+    d[-1]=(f[-1]-f[-2])/(x[-1]-x[-2])
+    
+    # solve tridiagonal eq. A*f=d
+    c[0]=c[0]/b[0]
+    d[0]=d[0]/b[0]
+    for i in range(1,len(x)-1):
+        temp=b[i]-c[i-1]*a[i]
+        c[i]=c[i]/temp
+        d[i]=(d[i]-d[i-1]*a[i])/temp
+    #end for
+        
+    fx[-1]=d[-1]
+    for i in range(len(x)-2,-1,-1):
+        fx[i]=d[i]-c[i]*fx[i+1]
+    #end for
+        
+    return fx
+# end function init_1d_spline
+
+""" 
+Add smoothing functions 
+
+def smooth1d(x,f,factor=3):
+    ...
+    ...
+    return ...
+
+def smooth2d(x,y,f,factor=3):
+    ...
+    ...
+    return ... 
+
+def smooth3d(x,y,z,f,factor=3):
+    ...
+    ...
+    ...
+    return ...
+"""
+
+class spline(object):
+
+    def __init__(self,*args,**kwargs):
+        self.dims=kwargs['dims']
+        if (self.dims==1):
+            self.x=kwargs['x']
+            self.f=kwargs['f']
+            self.hx=diff(self.x)
+            self.fx=init_1d_spline(self.x,self.f,self.hx)
+        elif (self.dims==2):
+            self.x=kwargs['x']
+            self.y=kwargs['y']
+            self.f=kwargs['f']
+            self.hx=diff(self.x)
+            self.hy=diff(self.y)
+            self.fx=zeros(shape(self.f))
+            self.fy=zeros(shape(self.f))
+            self.fxy=zeros(shape(self.f))
+            for i in range(max([len(self.x),len(self.y)])):
+                if (i<len(self.y)):
+                    self.fx[:,i]=init_1d_spline(self.x,self.f[:,i],self.hx)
+                if (i<len(self.x)):
+                    self.fy[i,:]=init_1d_spline(self.y,self.f[i,:],self.hy)
+            #end for
+            for i in range(len(self.y)):
+                self.fxy[:,i]=init_1d_spline(self.x,self.fy[:,i],self.hx)
+            #end for
+        elif (self.dims==3):
+            self.x=kwargs['x']
+            self.y=kwargs['y']
+            self.z=kwargs['z']
+            self.f=kwargs['f']
+            self.hx=diff(self.x)
+            self.hy=diff(self.y)
+            self.hz=diff(self.z)
+            self.fx=zeros(shape(self.f))
+            self.fy=zeros(shape(self.f))
+            self.fz=zeros(shape(self.f))
+            self.fxy=zeros(shape(self.f))
+            self.fxz=zeros(shape(self.f))
+            self.fyz=zeros(shape(self.f))
+            self.fxyz=zeros(shape(self.f))
+            for i in range(max([len(self.x),len(self.y),len(self.z)])):
+                for j in range(max([len(self.x),len(self.y),len(self.z)])):
+                    if (i<len(self.y) and j<len(self.z)):
+                        self.fx[:,i,j]=init_1d_spline(self.x,self.f[:,i,j],self.hx)
+                    if (i<len(self.x) and j<len(self.z)):
+                        self.fy[i,:,j]=init_1d_spline(self.y,self.f[i,:,j],self.hy)
+                    if (i<len(self.x) and j<len(self.y)):
+                        self.fz[i,j,:]=init_1d_spline(self.z,self.f[i,j,:],self.hz)
+            #end for
+            for i in range(max([len(self.x),len(self.y),len(self.z)])):
+                for j in range(max([len(self.x),len(self.y),len(self.z)])):
+                    if (i<len(self.y) and j<len(self.z)):
+                        self.fxy[:,i,j]=init_1d_spline(self.x,self.fy[:,i,j],self.hx)
+                    if (i<len(self.y) and j<len(self.z)):
+                        self.fxz[:,i,j]=init_1d_spline(self.x,self.fz[:,i,j],self.hx)
+                    if (i<len(self.x) and j<len(self.z)):
+                        self.fyz[i,:,j]=init_1d_spline(self.y,self.fz[i,:,j],self.hy)
+            #end for
+            for i in range(len(self.y)):
+                for j in range(len(self.z)):
+                    self.fxyz[:,i,j]=init_1d_spline(self.x,self.fyz[:,i,j],self.hx)
+            #end for
+        else:
+            print('Either dims is missing or specific dims is not available')
+        #end if
+
+    def p1(self, t):
+        return (1+2*t)*((t-1)**2)  
+    
+    def p2(self, t):
+        return t**2*(3-2*t)
+    def q1(self, t):
+        return t*(t-1)**2   
+    def q2(self, t):
+        return t**2*(t-1)
+            
+    def eval1d(self,x):
+        if isscalar(x):
+            # make sure that x is array
+            x=array([x])
+        N=len(self.x)-1
+        f=zeros((len(x),))
+        ii=0
+        for val in x:
+            # round and find the closest integer for i
+            i=floor(where(self.x<=val)[0][-1]).astype(int)
+            # when the axis reaches the maximum of the x, set the value of the
+            # f as the last value for interpolation
+            if i==N:
+                f[ii]=self.f[i]
+            # calculated according to the Hermite cubic splines
+            else:
+                t=(val-self.x[i])/self.hx[i]
+                f[ii]=self.f[i]*self.p1(t)+self.f[i+1]*self.p2(t)+self.hx[i]*(self.fx[i]*self.q1(t)+self.fx[i+1]*self.q2(t))
+            ii+=1
+
+        return f
+    #end eval1d
+    
+    def test1d(self, x, y, tol):
+        plt.figure()
+        plt.plot(y)
+        plt.title('error norm1')
+        plt.savefig('spline 1D error.pdf',dpi=200)
+        err = LA.norm(y,2)
+        print('the norm 2 of the difference between interpolation and analytic function:')
+        print(np.array(err))
+        if err < tol:
+            print('1D interpolate is OK')
+        return err
+
+    def eval2d(self,x,y):
+        # make sure that x and y are arrays
+        if isscalar(x):
+            x=array([x])
+        if isscalar(y):
+            y=array([y])
+        Nx=len(self.x)-1
+        Ny=len(self.y)-1
+        # initiate the f and A
+        f=zeros((len(x),len(y)))
+        A=zeros((4,4))
+        ii=0
+        for valx in x:
+        # round and find the closest integer for i
+            i=floor(where(self.x<=valx)[0][-1]).astype(int)
+            # when the axis reaches the maximum of the x,y, set the 
+            # value of h as the last value for interpolation            
+
+            if (i==Nx):
+                i-=1
+            jj=0
+            for valy in y:
+                j=floor(where(self.y<=valy)[0][-1]).astype(int)
+                if (j==Ny):
+                    j-=1
+                u = (valx-self.x[i])/self.hx[i]
+                v = (valy-self.y[j])/self.hy[j]
+                # calculate according to Hermite cubic splines 2D
+                pu = array([self.p1(u),self.p2(u),self.hx[i]*self.q1(u),self.hx[i]*self.q2(u)])
+                pv = array([self.p1(v),self.p2(v),self.hy[j]*self.q1(v),self.hy[j]*self.q2(v)])
+                A[0,:]=array([self.f[i,j],self.f[i,j+1],self.fy[i,j],self.fy[i,j+1]])
+                A[1,:]=array([self.f[i+1,j],self.f[i+1,j+1],self.fy[i+1,j],self.fy[i+1,j+1]])
+                A[2,:]=array([self.fx[i,j],self.fx[i,j+1],self.fxy[i,j],self.fxy[i,j+1]])
+                A[3,:]=array([self.fx[i+1,j],self.fx[i+1,j+1],self.fxy[i+1,j],self.fxy[i+1,j+1]])           
+                
+                f[ii,jj]=dot(pu,dot(A,pv))
+                jj+=1
+            ii+=1
+        return f
+    #end eval2d
+    
+    def test2d(self, x, y, z, tol):
+        fig2 = plt.figure()
+        ax21 = fig2.add_subplot(121, projection='3d')
+        ax22 = fig2.add_subplot(122)        
+        ax21.plot_wireframe(x, y, z)
+        # the norm2 of the error
+        plt.title('error norm2')
+        plt.savefig('spline 2D error.pdf',dpi=200)
+        ax22.pcolor(x, y, z)
+        # the colored norm2 of the error        
+        plt.title('colored error')
+        plt.savefig('colored_2D error.pdf',dpi=200)
+        err = LA.norm(z,2)
+        print('the norm 2 of the difference between interpolation and analytic function:')
+        print(np.array(err))
+        if err < tol:
+            print('2D interpolate is OK')
+        return err
+
+    def eval3d(self,x,y,z):
+        # round and find the closest integer for i
+        if isscalar(x):
+            x=array([x])
+        if isscalar(y):
+            y=array([y])
+        if isscalar(z):
+            z=array([z])
+        Nx=len(self.x)-1
+        Ny=len(self.y)-1
+        Nz=len(self.z)-1
+        # initiate f, A, B
+        f=zeros((len(x),len(y),len(z)))
+        A=zeros((4,4))
+        B=zeros((4,4))
+        ii=0
+        for valx in x:
+        # round and find the closest integer for i
+            i=floor(where(self.x<=valx)[0][-1]).astype(int)
+            # when the axis reaches the maximum of the x,y, set the 
+            # value of h as the last value for interpolation            
+            if (i==Nx):
+                i-=1
+            jj=0
+            for valy in y:
+                j=floor(where(self.y<=valy)[0][-1]).astype(int)
+                if (j==Ny):
+                    j-=1
+                kk=0
+                for valz in z:
+                    k=floor(where(self.z<=valz)[0][-1]).astype(int)
+                    if (k==Nz):
+                        k-=1
+                    # calculate according to Hermite cubic splines 2D
+                    u = (valx-self.x[i])/self.hx[i]
+                    v = (valy-self.y[j])/self.hy[j]
+                    t = (valz-self.z[k])/self.hz[k]
+                    pu = array([self.p1(u),self.p2(u),self.hx[i]*self.q1(u),self.hx[i]*self.q2(u)])
+                    pv = array([self.p1(v),self.p2(v),self.hy[j]*self.q1(v),self.hy[j]*self.q2(v)])
+                    pt = array([self.p1(t),self.p2(t),self.hz[k]*self.q1(t),self.hz[k]*self.q2(t)])
+                    # calculate A and B according to Hermite cubic splines 2D
+                    B[0,:]=array([self.f[i,j,k],self.f[i,j,k+1],self.fz[i,j,k],self.fz[i,j,k+1]])
+                    B[1,:]=array([self.f[i+1,j,k],self.f[i+1,j,k+1],self.fz[i+1,j,k],self.fz[i+1,j,k+1]])
+                    B[2,:]=array([self.fx[i,j,k],self.fx[i,j,k+1],self.fxz[i,j,k],self.fxz[i,j,k+1]])
+                    B[3,:]=array([self.fx[i+1,j,k],self.fx[i+1,j,k+1],self.fxz[i+1,j,k],self.fxz[i+1,j,k+1]])
+                    A[:,0]=dot(B,pt)
+                    B[0,:]=array([self.f[i,j+1,k],self.f[i,j+1,k+1],self.fz[i,j+1,k],self.fz[i,j+1,k+1]])
+                    B[1,:]=array([self.f[i+1,j+1,k],self.f[i+1,j+1,k+1],self.fz[i+1,j+1,k],self.fz[i+1,j+1,k+1]])
+                    B[2,:]=array([self.fx[i,j+1,k],self.fx[i,j+1,k+1],self.fxz[i,j+1,k],self.fxz[i,j+1,k+1]])
+                    B[3,:]=array([self.fx[i+1,j+1,k],self.fx[i+1,j+1,k+1],self.fxz[i+1,j+1,k],self.fxz[i+1,j+1,k+1]])
+                    A[:,1]=dot(B,pt)
+
+                    B[0,:]=array([self.fy[i,j,k],self.fy[i,j,k+1],self.fyz[i,j,k],self.fyz[i,j,k+1]])
+                    B[1,:]=array([self.fy[i+1,j,k],self.fy[i+1,j,k+1],self.fyz[i+1,j,k],self.fyz[i+1,j,k+1]])
+                    B[2,:]=array([self.fxy[i,j,k],self.fxy[i,j,k+1],self.fxyz[i,j,k],self.fxyz[i,j,k+1]])
+                    B[3,:]=array([self.fxy[i+1,j,k],self.fxy[i+1,j,k+1],self.fxyz[i+1,j,k],self.fxyz[i+1,j,k+1]])
+                    A[:,2]=dot(B,pt)
+                    B[0,:]=array([self.fy[i,j+1,k],self.fy[i,j+1,k+1],self.fyz[i,j+1,k],self.fyz[i,j+1,k+1]])
+                    B[1,:]=array([self.fy[i+1,j+1,k],self.fy[i+1,j+1,k+1],self.fyz[i+1,j+1,k],self.fyz[i+1,j+1,k+1]])
+                    B[2,:]=array([self.fxy[i,j+1,k],self.fxy[i,j+1,k+1],self.fxyz[i,j+1,k],self.fxyz[i,j+1,k+1]])
+                    B[3,:]=array([self.fxy[i+1,j+1,k],self.fxy[i+1,j+1,k+1],self.fxyz[i+1,j+1,k],self.fxyz[i+1,j+1,k+1]])
+                    A[:,3]=dot(B,pt)
+                
+                    f[ii,jj,kk]=dot(pu,dot(A,pv))
+                    kk+=1
+                jj+=1
+            ii+=1
+        return f
+    #end eval3d
+    
+    def test3d(self, X, Y, Z, tol):
+        fig3 = plt.figure()
+        ax3 = fig3.add_subplot(111)        
+        ax3.pcolor(X, Y, Z[...,int(np.size(Z,2)/2)])
+        # the colored norm2 of the error        
+        plt.title('colored error')
+        plt.savefig('spline_colored_3D error.pdf',dpi=200)
+        err = LA.norm(Z[...,int(np.size(Z,2)/2)],2)
+        print('the norm 2 of the difference between interpolation and analytic function:')
+        print(np.array(err))
+        if err < tol:
+            print('3D interpolate is OK')
+        return err
+#end class spline
+
+
+    
+def main():
+
+    fig1d = plt.figure()
+    ax1d = fig1d.add_subplot(111)
+    # 1d example
+    x=np.linspace(0.,2.*np.pi,11)
+    # test with sin
+    def fun(x):
+        return np.sin(x)
+    y=fun(x)
+    spl1d=spline(x=x,f=y,dims=1)
+    xx=np.linspace(0.,2.*np.pi,101)
+    ax1d.plot(xx,spl1d.eval1d(xx))   
+    ax1d.plot(x,y,'o',xx,np.sin(xx),'r--')
+    ax1d.set_title('interpolation of f')
+    plt.savefig('spline 1D.pdf',dpi=200)
+    tol = 0.5
+    err1 = spl1d.test1d(xx, spl1d.eval1d(xx)-np.sin(xx), tol)
+    
+    # 2d example
+    fig2d = plt.figure()
+    #set the axis for four subplot separately
+    ax2d = fig2d.add_subplot(221, projection='3d')
+    ax2d2 = fig2d.add_subplot(222, projection='3d')
+    ax2d3 = fig2d.add_subplot(223)
+    ax2d4 = fig2d.add_subplot(224)
+
+    x=np.linspace(-2.0,2.0,11)
+    y=np.linspace(-2.0,2.0,11)
+    X,Y = np.meshgrid(x,y)
+    # test function is Z
+    def fun2(X,Y):
+        return X*np.exp(-1.0*(X*X+Y*Y))
+    Z1 = fun2(X,Y)
+    # plot interpolation 2D with 11*11 grids
+    ax2d.plot_wireframe(X,Y,Z1)
+    plt.title('interpolation 2D with 11*11 grids')
+    plt.savefig('spline_2D_11_11.pdf',dpi=200)
+    # colored with z_value
+    ax2d3.pcolor(X,Y,Z1)
+    plt.title('color of interpolation 2D with 11*11 grids')
+    plt.savefig('spline_colored_2D_11_11.pdf',dpi=200)    
+    #ax2d3.contourf(X,Y,Z)
+    
+    spl2d=spline(x=x,y=y,f=Z1,dims=2)
+    x=np.linspace(-2.0,2.0,51)
+    y=np.linspace(-2.0,2.0,51)
+    X,Y = np.meshgrid(x,y)
+    Z2 = spl2d.eval2d(x,y)
+    # plot interpolation 2D with 51*51 grids     
+    ax2d2.plot_wireframe(X,Y,Z2)
+    plt.title('interpolation 2D with 51*51 grids')
+    plt.savefig('spline_2D_51_51.pdf',dpi=200)
+    # colored with z_value
+    ax2d4.pcolor(X,Y,Z2)
+    plt.title('color of interpolation 2D with 51*51 grids')
+    plt.savefig('spline_colored_2D_51_51.pdf',dpi=200)
+    tol = 1    
+    err2 = spl2d.test2d(X, Y, Z2-fun2(X,Y), tol)
+
+    # 3d example
+    x=np.linspace(0.0,3.0,11)
+    y=np.linspace(0.0,3.0,11)
+    z=np.linspace(0.0,3.0,11)
+    X,Y,Z = np.meshgrid(x,y,z)
+    def fun3(X,Y,Z):
+        return (X+Y+Z)*np.exp(-1.0*(X*X+Y*Y+Z*Z))
+    F1 = fun3(X,Y,Z)
+    X,Y= np.meshgrid(x,y)
+    fig3d=plt.figure()
+    ax3d=fig3d.add_subplot(121)
+    ax3d.pcolor(X,Y,F1[...,int(len(z)/2)])
+    spl3d=spline(x=x,y=y,z=z,f=F1,dims=3)
+    
+    x=np.linspace(0.0,3.0,50)
+    y=np.linspace(0.0,3.0,50)
+    z=np.linspace(0.0,3.0,50)
+    X,Y,Z = np.meshgrid(x,y,z) 
+    F1 = fun3(X,Y,Z)
+    X,Y= np.meshgrid(x,y)
+    F2 = spl3d.eval3d(x,y,z)
+    ax3d2=fig3d.add_subplot(122)
+    ax3d2.pcolor(X,Y,F2[...,int(len(z)/2)]) 
+    tol = 15      
+    err3 = spl3d.test3d(X, Y, F2-F1, tol)
+
+    plt.show()
+#end main
+    
+if __name__=="__main__":
+    main()
