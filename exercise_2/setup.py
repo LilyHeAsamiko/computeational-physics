159e598dd8191ddc5e20337b06bfcfcee0a472ec
Qin He(Lily) <qin.he@tut.fi>
2019-01-20 01:56:11 +0000

    Upload New File

---
+++ exercise_2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 00:52:30 2019
+
+@author: user
+"""
+from setuptools import setup
+
+setup(name='exercise_2',
+      version='0.1',
+      description='The exercise 2 for computational course',
+      url='https://gitlab.com/LilyHeAsamiko1/computational-physics/tree/master/exercise_2',
+      author='Qin He',
+      author_email='qin.he@tuni.fi',
+      license='MIT',
+      packages=['exercise_2'],
+      zip_safe=False)
